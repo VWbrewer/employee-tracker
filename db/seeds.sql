9086d77dfb86e31063e69138d9fcfaa611ccbcfe
cbrewer <144485839+VWbrewer@users.noreply.github.com>
2023-12-12 15:32:11 -0600

    update roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,19 @@
 INSERT INTO department (id, name)
-VALUES  (010, "Marketing"),
-        (020, "Events"),
-        (030, "Engineering"),
-        (040, "Community");
+VALUES  (010, "Management"),
+        (020, "HR"),
+        (030, "Marketing"),
+        (040, "IT"),
+        (050, "Sales");
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES  (011, "Marketing Lead", 150000, 010),
-        (012, "Campaign Manager", 95000, 010),
-        (021, "Events Lead", 150000, 020),
-        (022, "Event Planner", 95000, 020),
-        (031, "Engineering Lead", 150000, 030),
-        (032, "Database Engineer", 95000, 030),
-        (041, "Community Lead", 150000, 040),
-        (042, "Commnity Strategist", 95000, 040);
+VALUES  (011, "Debarment Manager", 250000, 010),
+        (012, "HR Supervisor", 18000, 020),
+        (021, "Marketing Supervisor", 150000, 030),
+        (022, "HR", 100000, 020),
+        (031, "IT Advisor", 150000, 040),
+        (032, "IT Engineer", 92000, 040),
+        (041, "Sales Lead", 80000, 050),
+        (042, "Sales Team", 50000, 050);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES  (111, "Sofie", "Lopez", 011,  1),
